0effafc2ad40329fcd071c0d6776ff77dadaa595
Connor <63462198+itsrucky@users.noreply.github.com>
2020-12-23 17:26:42 -0500

    Add files via upload

---
+++ termulator 2.0.py
@@ -0,0 +1,12 @@
+num1 = int(input("num1: "))
+op = input("Operator: ")
+num2 = int(input("num2: "))
+
+if op == "+":
+     print(num1 + num2)
+if op == "-":
+     print(num1 - num2)
+if op == "/":
+     print(num1 / num2)
+if op == "*":
+     print(num1 * num2)
